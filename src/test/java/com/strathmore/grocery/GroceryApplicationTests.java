04f08a15084ea766666873c813169efeddb542b7
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-05-25 17:40:45 +0300

    Creation of the SpringBoot Framework
    The main skeleton framework of the springboot project is created.

---
+++ src/test/java/com/strathmore/grocery/GroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strathmore.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
