782070eabecb37613c2a4a35de6a176ba4637a9f
kylemwastaken <kyle.mokua@strathmore.edu>
2025-05-26 09:59:27 +0300

    Update OrderRepository.java

--- src/main/java/com/strathmore/grocery/repositories/OrderRepository.java
+++ src/main/java/com/strathmore/grocery/repositories/OrderRepository.java
@@ -1,5 +1,4 @@
 package com.strathmore.grocery.repositories;
 
-public interface OrderRepository extends JpaRepository<CartItem,Long> {
-  Page<CartItem> findByNameContainingIgnoreCase(String name, Pageable pageable);
+  public class OrderRepository {
 }
