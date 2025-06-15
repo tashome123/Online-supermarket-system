cbede980ea34083cc20aeaf4779ae43f41be3ded
kylemwastaken <kyle.mokua@strathmore.edu>
2025-06-15 22:29:53 +0300

    Update Payment.java

--- src/main/java/com/strathmore/grocery/models/Payment.java
+++ src/main/java/com/strathmore/grocery/models/Payment.java
@@ -1,4 +1,15 @@
 package com.strathmore.grocery.models;
 
+@Entity
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;  
+    private String paymentMethod;
+    private Double totalamount;
+    private LocalDate paymentDate;
+
+    
 }
