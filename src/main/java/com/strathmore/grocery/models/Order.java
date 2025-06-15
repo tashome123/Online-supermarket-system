56a4daa91197a446b1a753fd0c24fa37e6dd52db
kylemwastaken <kyle.mokua@strathmore.edu>
2025-06-15 22:24:26 +0300

    Update Order.java

--- src/main/java/com/strathmore/grocery/models/Order.java
+++ src/main/java/com/strathmore/grocery/models/Order.java
@@ -1,4 +1,15 @@
 package com.strathmore.grocery.models;
 
+  @Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userid;
+    private LocalDate orderDate;
+    private Double totalAmount;
+
+    
 }
+
